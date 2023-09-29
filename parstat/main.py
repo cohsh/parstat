[fix] Changed the case classification in the distribution functions to avoid overflow.

parstat/main.py
@@ -4,6 +4,8 @@ import numpy as np
 float_min = sys.float_info.min
 float_max = sys.float_info.max
 
+ln_max = np.log(float_max)
+
 def boltzmann_distribution(temperature: float, energy: float) -> float:
     # Calculates the occupation number of particles that follow the Blotzmann distribution.
 
@@ -12,24 +14,24 @@ def boltzmann_distribution(temperature: float, energy: float) -> float:
 
     if kbt > float_min:
         beta = 1.0 / kbt  # inverse temperature
-        x = beta * energy
-        n = np.exp(-x)
-        if n == np.inf:
-            n = float_max
+        ln = - beta * energy
+        if ln <= ln_max:
+            n = np.exp(ln)
+        else:
+            n = np.sqrt(float_max)
     else:
         if energy > 0.0:
             n = 0.0
         elif energy == 0.0:
             n = 1.0
         else:
-            n = float_max
+            n = np.sqrt(float_max)
     return n
 
 def fermi_distribution(temperature: float, energy: float) -> float:
     # Calculates the occupation number of particles that follow the Fermi-Dirac distribution.
 
     boltzmann_factor = boltzmann_distribution(temperature, energy)
-    n = 0.0
     n = boltzmann_factor / (1.0 + boltzmann_factor)
     return n
 
@@ -40,7 +42,7 @@ def bose_distribution(temperature: float, energy: float) -> float:
     if boltzmann_factor != 1.0:
         n = boltzmann_factor / (1.0 - boltzmann_factor)
     else:
-        n = float_max
+        n = np.nan
     return n
 
 def gaussian_distribution(sigma: float, energy: float) -> float:

samples/plot_bose_distribution.py
@@ -5,18 +5,20 @@ import matplotlib.pyplot as plt
 import parstat
 
 def main():
-    temperature = 300.0
-
+    energy_min = 0.001
     energy_max = 0.01
 
-    energy = np.linspace(0.001, energy_max, 1000)
+    energy = np.linspace(energy_min, energy_max, 1000)
 
-    n_bose = np.empty(0)
+    for i in range(5):
+        temperature = i * 100.0
+        n_bose = np.empty(0)
+        for e in energy:
+            n_bose = np.append(n_bose, parstat.bose_distribution(temperature, e))
 
-    for e in energy:
-        n_bose = np.append(n_bose, parstat.bose_distribution(temperature, e))
-
-    plt.plot(energy, n_bose, label="Bose Distribution")
+        plt.plot(energy, n_bose, label="{0} K".format(int(temperature)))
+    
+    plt.title("Bose Distribution")
     plt.xlabel("Energy ($\mathrm{Hartree}$)")
     plt.ylabel("Occupation Number")
     plt.legend()

samples/plot_fermi_distribution.py
@@ -5,18 +5,19 @@ import matplotlib.pyplot as plt
 import parstat
 
 def main():
-    temperature = 300.0
-
     energy_max = 0.01
 
-    energy = np.linspace(-energy_max, energy_max, 1000)
+    energy = np.linspace(-energy_max, energy_max, 10000)
 
-    n_fermi = np.empty(0)
+    for i in range(5):
+        temperature = i * 100.0
+        n_fermi = np.empty(0)
+        for e in energy:
+            n_fermi = np.append(n_fermi, parstat.fermi_distribution(temperature, e))
 
-    for e in energy:
-        n_fermi = np.append(n_fermi, parstat.fermi_distribution(temperature, e))
+        plt.plot(energy, n_fermi, label="{0} K".format(int(temperature)))
 
-    plt.plot(energy, n_fermi, label="Fermi Distribution")
+    plt.title("Fermi Distribution")
     plt.xlabel("Energy ($\mathrm{Hartree}$)")
     plt.ylabel("Occupation Number")
     plt.legend()